6efd2a240891b0ddcffb321b508d7c46ed82cf63
gendzmin <gendzmin@gmail.com>
2023-03-24 13:35:31 +0300

    fixing GitHub Actions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,7 @@ import { test, expect } from '@jest/globals';
 import getDiff from '../src/index.js';
 
 test('get difference / usual', () => {
-    expect(getDiff('/home/gendzmin/frontend-project-46/__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual('{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}');
+    expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual('{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}');
 });
 
 test('get difference / empty files', () => {
