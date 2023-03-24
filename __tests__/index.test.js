fb5b13e9f580c911d0df1959d7bdbfa64e6107ae
gendzmin <gendzmin@gmail.com>
2023-03-24 12:52:46 +0300

    updating tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,9 +4,9 @@ import { test, expect } from '@jest/globals';
 import getDiff from '../src/index.js';
 
 test('get difference / usual', () => {
-    expect(getDiff('/home/gendzmin/frontend-project-46/__fixtures__/file1.json', '__fixtures__/file2.json', 'type')).toEqual('{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}');
+    expect(getDiff('/home/gendzmin/frontend-project-46/__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual('{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}');
 });
 
 test('get difference / empty files', () => {
-    expect(getDiff('__fixtures__/emptyFile1.json', '__fixtures__/emptyFile2.json', 'type')).toEqual('Files are empty!');
+    expect(getDiff('__fixtures__/emptyFile1.json', '__fixtures__/emptyFile2.json')).toEqual('Files are empty!');
 });
