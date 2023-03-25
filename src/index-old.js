a84329d2f0e17d116633cb35c6589842208c0cb0
gendzmin <gendzmin@gmail.com>
2023-03-25 15:00:50 +0300

    improving maintainability

--- src/index-old.js
+++ src/index-old.js
@@ -20,15 +20,15 @@ const isObjectEmpty = (obj) => {
 
 const getDiffLine = (key, keys1, keys2, file1, file2) => {
   if (keys1.includes(key) && !(keys2.includes(key))) { // Если ключ есть в 1-м, но нет во 2-м файле
-    return `- ${key}: ${file1[key]}`;
+    return '1only';
   }
   if (!(keys1.includes(key)) && keys2.includes(key)) { // Если ключ есть во 2-м, но нет во 1-м файле
-    return `+ ${key}: ${file2[key]}`;
+    return '2only';
   }
   if ((file1[key] === file2[key])) { // Если значения по ключу одинаковые
-    return `  ${key}: ${file2[key]}`;
+    return 'identical';
   }
-  return `- ${key}: ${file1[key]}\n+ ${key}: ${file2[key]}`; // Если значения по ключу одинаковые
+  return 'different'; // Если значения по ключу одинаковые
 };
 
 const getDiff = (filepath1, filepath2) => {
@@ -41,7 +41,21 @@ const getDiff = (filepath1, filepath2) => {
   const keys2 = Object.keys(file2); // Массив ключей второго файла
   const keys = _.union(keys1, keys2).sort(); // Массив ключей обоих файлов в алфавитном порядке
   const result = keys.reduce((current, key) => {
-    current.push(getDiffLine(key, keys1, keys2, file1, file2));
+    const comparison = getDiffLine(key, keys1, keys2, file1, file2);
+    switch (comparison) {
+      case '1only':
+        current.push(`- ${key}: ${file1[key]}`);
+        break;
+      case '2only':
+        current.push(`+ ${key}: ${file2[key]}`);
+        break;
+      case 'identical':
+        current.push(`  ${key}: ${file2[key]}`);
+        break;
+      default:
+        current.push(`- ${key}: ${file1[key]}\n+ ${key}: ${file2[key]}`);
+        break;
+    }
     return current;
   }, []);
   return makeString(result);
