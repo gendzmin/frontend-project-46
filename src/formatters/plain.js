b237f9e8a3082cee8a21e61aeeecc7c154a4d3f1
gendzmin <gendzmin@gmail.com>
2023-04-21 12:27:40 +0300

    fixing plain formatter complexity

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import iterateValue from '../tree';
 
-const objecter = (file, key) => {
+const getValue = (file, key) => {
   if (_.isObject(file[key])) {
     return '[complex value]';
   }
@@ -11,19 +11,21 @@ const objecter = (file, key) => {
   return file[key];
 };
 
+const getPath = (path, key) => (path === '' ? `${key}` : `${path}.${key}`);
+
 const makePlain = (id, key, file1, file2, path) => {
-  const currentPath = (path === '') ? `${key}` : `${path}.${key}`;
+  const currentPath = getPath(path, key);
   let result;
   if (id.presence === 'first') { // Если значение по ключу есть только в первом объекте
     result = `Property '${currentPath}' was removed`;
   } else if (id.presence === 'second') { // Если значение по ключу есть только во втором объекте
-    result = `Property '${currentPath}' was added with value: ${objecter(file2, key)}`;
+    result = `Property '${currentPath}' was added with value: ${getValue(file2, key)}`;
   } else if (id.equality === 'equal') {
     return 'empty';
   } else if (id.type === 'both-obj') { // Если значения по ключу неравны, но оба являются обектами
     result = `${iterateValue('plain', file1[key], file2[key], currentPath)}`;
   } else if (id.type === 'not-both-obj') { // Если значения по ключу неравны, но не являются объектами
-    result = `Property '${currentPath}' was updated. From ${objecter(file1, key)} to ${objecter(file2, key)}`;
+    result = `Property '${currentPath}' was updated. From ${getValue(file1, key)} to ${getValue(file2, key)}`;
   }
   return result;
 };
